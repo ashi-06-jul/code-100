58473ae438a880dc3ef58380d8cbebf13cab5c7c
Ashi Anand <ashianand21@gmail.com>
2018-08-04 00:12:57 +0530

    python-day-0

---
+++ code/python/day-0/Numbers.py
@@ -0,0 +1,19 @@
+print(2)
+print(3.9)
+print(-2.3)
+
+# operations
+
+print(2/3)
+print(3*4+2)
+print(3*(4+2))
+
+print(10%3)
+
+#print favorite number
+
+my_fav = 10
+
+#print(my_fav+" is my favorite number")
+
+print(str(my_fav)+" is my favorite number")
