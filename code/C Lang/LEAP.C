ec5f896048ef1907ff31d0a59409c1c7410a6feb
Ashi Anand <ashianand21@gmail.com>
2018-09-07 22:15:05 +0530

    leap-yr

---
+++ code/C Lang/LEAP.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int year;
+clrscr();
+scanf("%d",&year);
+if((year%4==0 && year%100!=0)||(year%400==0))
+printf("year %d is a leap year",year);
+else
+printf("NOT leap year");
+getch();
+}
