a27feb70fe94acb8383f61afffb8e0cdcb7c172f
Ashi Anand <ashianand21@gmail.com>
2018-09-14 23:00:58 +0530

    java

---
+++ code/java/TypeCasting.java
@@ -0,0 +1,12 @@
+public class TypeCasting
+{
+    public static void main(String args[])
+    {
+        float f=10.5f;
+        int a=(int)f;
+        System.out.println(f);
+        System.out.println(a);
+
+
+    }
+}
