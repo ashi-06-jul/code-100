0d159dd07a56f321715a85cce902b1d6378dd929
Ashi Anand <ashianand21@gmail.com>
2018-09-17 23:03:06 +0530

    java

---
+++ code/java/Switch2.java
@@ -0,0 +1,14 @@
+public class Switch2
+{
+    public static void main(String args[])
+    {
+        int n=20;
+        switch(n)
+        {
+        case 10: System.out.println("Sagar");
+        case 20: System.out.println("Garima");
+        case 30: System.out.println("Senger");
+        default: System.out.println("Not in 10");
+        }
+    }
+}
