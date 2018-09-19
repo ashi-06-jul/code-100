1440ffe44c87d0b4baa941e753752e84818cf98f
Ashi Anand <ashianand21@gmail.com>
2018-09-19 21:45:21 +0530

    java

---
+++ code/java/ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop
+{
+public static void main(String args[])
+{
+    int arr[]={12,32,32,4,53,43,22,34};
+    for(int i:arr)
+    System.out.println(i);
+}
+}
---
+++ code/java/If.java
@@ -0,0 +1,15 @@
+public class If
+{
+    public static void main(String[] args)
+    {
+        int age=2;
+        if(age>18)
+        {
+        System.out.println("Age is greater than 18");
+        }
+        else
+        {
+            System.out.println("Age is not greater than 18");
+        }
+    }
+}
