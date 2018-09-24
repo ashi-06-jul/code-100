2d35a123ad17aa811830f7b88e02e42648520361
Ashi Anand <ashianand21@gmail.com>
2018-09-24 23:16:51 +0530

    day-66

---
+++ code/java/Constructor1.java
@@ -0,0 +1,11 @@
+public class Constructor1
+{
+    Constructor1()
+    {
+        System.out.println("Constructor is created");
+    }
+    public static void main(String args[])
+    {
+Constructor1 obj=new Constructor1();
+    }
+}
