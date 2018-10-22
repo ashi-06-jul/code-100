704037cb2b3d6bd53c7d39f884b9bd9baf1b9824
ashi-06-jul <36784332+ashi-06-jul@users.noreply.github.com>
2018-10-22 23:24:02 +0530

    Update Palindrome.java

--- code/java/day-3/Palindrome.java
+++ code/java/day-3/Palindrome.java
@@ -14,6 +14,6 @@ class Palindrome
   if(temp==sum)    
    System.out.println("palindrome number");    
   else    
-   System.out.println("not palindrome");    
+   System.out.println("not palindrome number");    
 }  
 }  
