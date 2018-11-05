8af58fac15bd25eb5816b548c93c1cba549009a8
ashi-06-jul <36784332+ashi-06-jul@users.noreply.github.com>
2018-11-05 23:39:30 +0530

    Update LEAP.C

--- code/C Lang/LEAP.C
+++ code/C Lang/LEAP.C
@@ -6,7 +6,7 @@ int year;
 clrscr();
 scanf("%d",&year);
 if((year%4==0 && year%100!=0)||(year%400==0))
-printf("Year %d is a leap year!",year);
+printf("Year %d is a leap year!!",year);
 else
 printf("Not leap year!!");
 getch();
